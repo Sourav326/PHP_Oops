09e2582dc86fd9aef1165bfec53cd3ebc5bb0ad0
Sourav Chauhan <souravchauhan1964@gmail.com>
2023-08-13 12:57:59 +0530

    create new object for calculation class

--- classObject.php
+++ classObject.php
@@ -13,4 +13,9 @@ $calculation->a = 10;
 $calculation->b = 25;
 echo $calculation->sum();
 
+$calculationTwo = new Calculation;
+$calculationTwo->a=34.6789;
+$calculationTwo->b=-12.3456;
+echo "<br> Second Object<br>".$calculationTwo->sum();
+
 ?>
